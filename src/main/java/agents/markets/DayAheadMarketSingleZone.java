e934321585185042dcd7785ff3461cbc02eb96ae
Christoph Schimeczek <Christoph.Schimeczek@dlr.de>
2023-12-11 15:22:04 +0100

    Extract abstract DayAheadMarket class

--- src/main/java/agents/markets/DayAheadMarketSingleZone.java
+++ src/main/java/agents/markets/DayAheadMarketSingleZone.java
@@ -5,58 +5,25 @@ package agents.markets;
 
 import java.util.ArrayList;
 import java.util.List;
-import agents.markets.meritOrder.MarketClearing;
 import agents.markets.meritOrder.MarketClearingResult;
 import agents.markets.meritOrder.books.DemandOrderBook;
 import agents.markets.meritOrder.books.OrderBook;
-import agents.markets.meritOrder.books.OrderBook.DistributionMethod;
 import agents.markets.meritOrder.books.OrderBookItem;
 import agents.markets.meritOrder.books.SupplyOrderBook;
 import agents.trader.Trader;
 import communications.message.AwardData;
-import communications.message.ClearingTimes;
-import de.dlr.gitlab.fame.agent.Agent;
 import de.dlr.gitlab.fame.agent.input.DataProvider;
-import de.dlr.gitlab.fame.agent.input.Input;
-import de.dlr.gitlab.fame.agent.input.Make;
-import de.dlr.gitlab.fame.agent.input.ParameterData;
 import de.dlr.gitlab.fame.agent.input.ParameterData.MissingDataException;
-import de.dlr.gitlab.fame.agent.input.Tree;
 import de.dlr.gitlab.fame.communication.Contract;
-import de.dlr.gitlab.fame.communication.Product;
 import de.dlr.gitlab.fame.communication.message.Message;
-import de.dlr.gitlab.fame.service.output.Output;
-import de.dlr.gitlab.fame.time.TimeSpan;
 import de.dlr.gitlab.fame.time.TimeStamp;
 
 /** Performs market clearing for a single day-ahead energy market zone
  * 
  * @author Christoph Schimeczek, Johannes Kochems */
-public class DayAheadMarketSingleZone extends Agent {
-	static final String LONE_LIST = "At most one element is expected in this list: ";
-	
-	@Product
-	public static enum Products {
-		/** Awarded energy and price per bidding trader */
-		Awards,
-		/** Information on when the market clearing is performed */
-		GateClosureInfo
-	};
-
-	@Output
-	private static enum OutputFields {
-		TotalAwardedPowerInMW, ElectricityPriceInEURperMWH, DispatchSystemCostInEUR
-	};
-
-	@Input private static final Tree parameters = Make.newTree()
-			.add(Make.newEnum("DistributionMethod", DistributionMethod.class),
-					 Make.newInt("GateClosureInfoOffsetInSeconds")).buildTree();
-
+public class DayAheadMarketSingleZone extends DayAheadMarket {
 	private DemandOrderBook demandBook = new DemandOrderBook();
 	private SupplyOrderBook supplyBook = new SupplyOrderBook();
-	private final MarketClearing marketClearing;
-	private final TimeSpan gateClosureInfoOffset;
-	private ClearingTimes clearingTimes;
 
 	/** Creates an {@link DayAheadMarketSingleZone}
 	 * 
@@ -64,25 +31,8 @@ public class DayAheadMarketSingleZone extends Agent {
 	 * @throws MissingDataException if any required data is not provided */
 	public DayAheadMarketSingleZone(DataProvider dataProvider) throws MissingDataException {
 		super(dataProvider);
-		ParameterData input = parameters.join(dataProvider);
-		marketClearing = new MarketClearing(input.getEnum("DistributionMethod", DistributionMethod.class));
-		gateClosureInfoOffset = new TimeSpan(input.getInteger("GateClosureInfoOffsetInSeconds"));
-
-		/**Sends out ClearingTimes*/
-		call(this::sendGateClosureInfo).on(Products.GateClosureInfo);
 		/** Clears market by using incoming bids and sending Awards */
 		call(this::clearMarket).on(Products.Awards).use(Trader.Products.Bids);
-	}
-	
-	/** Sends info upon next gate closure to connected traders
-	 * 
-	 * @param input n/a
-	 * @param contracts connected traders to inform */
-	private void sendGateClosureInfo(ArrayList<Message> input, List<Contract> contracts) {
-		clearingTimes = new ClearingTimes(now().laterBy(gateClosureInfoOffset));
-		for (Contract contract : contracts) {
-			fulfilNext(contract, clearingTimes);
-		}
 	}
 
 	/** Clears the market based on all the bids provided; writes out some market-clearing data
