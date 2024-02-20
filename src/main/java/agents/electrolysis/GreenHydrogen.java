3067451a067d35f2b6706a0864070cd17bc46999
Willeke <leonard.willeke@dlr.de>
2024-02-20 14:00:13 +0100

    Create new electrolyzer strategist GreenHydrogen

--- src/main/java/agents/electrolysis/GreenHydrogen.java
+++ src/main/java/agents/electrolysis/GreenHydrogen.java
@@ -1,3 +1,4 @@
+<<<<<<< Upstream, based on origin/dev
 // SPDX-FileCopyrightText: 2024 German Aerospace Center <amiris@dlr.de>
 //
 // SPDX-License-Identifier: Apache-2.0
@@ -68,6 +69,51 @@ public class GreenHydrogen extends ElectrolyzerStrategist {
 			scheduledChargedHydrogenTotal[hour] = initialHydrogenProductionInMWH;
 			initialHydrogenProductionInMWH += electrolyzer.calcHydrogenEnergy(demandScheduleInMWH[hour]);
 		}
+=======
+package agents.electrolysis;
+
+import agents.markets.meritOrder.sensitivities.MeritOrderSensitivity;
+import de.dlr.gitlab.fame.agent.input.Make;
+import de.dlr.gitlab.fame.agent.input.ParameterData;
+import de.dlr.gitlab.fame.agent.input.Tree;
+import de.dlr.gitlab.fame.agent.input.ParameterData.MissingDataException;
+import de.dlr.gitlab.fame.time.TimePeriod;
+import util.Polynomial;
+
+public class GreenHydrogen extends ElectrolyzerStrategist {
+	public static enum TemporalCorrelationPeriod {
+		HOURLY, MONTHLY,
+	}
+
+	/** Inputs specific to {@link GreenHydrogen} electrolyzer strategists */
+	public static final Tree parameters = Make.newTree()
+			.add(Make.newEnum("TemporalCorrelationPeriod", TemporalCorrelationPeriod.class).help(
+					"Period in which electrolyzer production must match production of associated renewable plant"))
+			.buildTree();
+	
+	private final TemporalCorrelationPeriod temporalCorrelationPeriod;
+
+	/** Create new {@link GreenHydrogen}
+	 * 
+	 * @param generalInput parameter group associated with flexibility strategists in general
+	 * @param specificInput parameter group associated with this strategist in specific
+	 * @throws MissingDataException if any required input data is missing */
+	public GreenHydrogen(ParameterData generalInput, ParameterData specificInput) throws MissingDataException {
+		super(generalInput);
+		temporalCorrelationPeriod = specificInput.getEnum("TemporalCorrelationPeriod", TemporalCorrelationPeriod.class);
+	}
+	
+	@Override
+	protected MeritOrderSensitivity createBlankSensitivity() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void updateSchedule(TimePeriod timePeriod) {
+		// TODO Auto-generated method stub
+
+>>>>>>> c90d9ce Create new electrolyzer strategist GreenHydrogen
 	}
 
 }
