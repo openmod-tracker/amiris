5ce8e0fdc837c438de59598ec33ddb2aa86071bc
sper_ev <evelyn.sperber@dlr.de>
2024-10-30 14:22:15 +0100

    use GateClosureInfo when requesting electricity forecast

--- src/main/java/agents/trader/HeatPumpTrader.java
+++ src/main/java/agents/trader/HeatPumpTrader.java
@@ -99,7 +99,7 @@ public class HeatPumpTrader extends FlexibilityTrader {
 		call(this::requestElectricityForecast).on(Products.MeritOrderForecastRequest);
 		call(this::updateMeritOrderForecast).on(Forecaster.Products.MeritOrderForecast)
 				.use(Forecaster.Products.MeritOrderForecast);
-		call(this::requestElectricityForecast).on(Products.PriceForecastRequest);
+		call(this::requestElectricityForecast).on(Products.PriceForecastRequest).use(DayAheadMarket.Products.GateClosureInfo);
 		call(this::updateElectricityPriceForecast).on(Forecaster.Products.PriceForecast)
 				.use(Forecaster.Products.PriceForecast);
 		call(this::prepareBids).on(DayAheadMarketTrader.Products.Bids).use(DayAheadMarket.Products.GateClosureInfo);
