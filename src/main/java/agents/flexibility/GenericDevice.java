82eb1fc1c349e88c11d3f343464e2eaa7f98335c
Felix Nitsch <felix.nitsch@dlr.de>
2025-03-12 16:15:28 +0100

    :memo:(GenericDevice) add unit to docstring

--- src/main/java/agents/flexibility/GenericDevice.java
+++ src/main/java/agents/flexibility/GenericDevice.java
@@ -65,7 +65,8 @@ public class GenericDevice {
 	/** @param time of transition
 	 * @param initialEnergyContentInMWH at the beginning of transition
 	 * @param duration of the transition
-	 * @return maximum allowed energy content for given initial energy content considering charging power and energy bounds */
+	 * @return maximum allowed energy content for given initial energy content considering charging power and energy bounds in
+	 *         MWh */
 	public double getMaxTargetEnergyContentInMWH(TimeStamp time, double initialEnergyContentInMWH, TimeSpan duration) {
 		double netChargingEnergyInMWH = (chargingPowerInMW.getValueLinear(time) + netInflowPowerInMW.getValueLinear(time))
 				* calcDurationInHours(duration);
@@ -84,7 +85,8 @@ public class GenericDevice {
 	/** @param time of transition
 	 * @param initialEnergyContentInMWH at the beginning of transition
 	 * @param duration of the transition
-	 * @return minimum allowed energy content for given initial energy content considering charging power and energy bounds */
+	 * @return minimum allowed energy content for given initial energy content considering charging power and energy bounds in
+	 *         MWh */
 	public double getMinTargetEnergyContentInMWH(TimeStamp time, double initialEnergyContentInMWH, TimeSpan duration) {
 		double netDischargingEnergyInMWH = (netInflowPowerInMW.getValueLinear(time)
 				- dischargingPowerInMW.getValueLinear(time)) * calcDurationInHours(duration);
